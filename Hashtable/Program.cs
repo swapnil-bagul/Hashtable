bd6ecbece2e12f1ecb2ef047ee93f6bfe6699673
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-07 17:31:42 +0530

    [Swapnil] Added UC1 Ability to Find Frequency

--- Hashtable/Program.cs
+++ Hashtable/Program.cs
@@ -11,6 +11,29 @@ namespace Hashtable
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string sentence = "To be or not to be";
+            CountWordFrequency(sentence );
+            Console.ReadLine();
+        }
+        public static void CountWordFrequency(string sentence)
+        {
+            MyHashCode<string, int> hashtable = new MyHashCode<string, int>(6);
+            string[] words = sentence.Split(' ');
+            foreach (string word in words)
+            {
+                if (hashtable.Exists(word))
+                {
+                    hashtable.Add(word, hashtable.get(word) + 1);
+
+                }
+                else
+                {
+                    hashtable.Add(word, 1);
+                }
+            }
+            Console.WriteLine("Displaying after add operations");
+            hashtable.Display();
+            Console.WriteLine("=======================================");
         }
     }
 }
